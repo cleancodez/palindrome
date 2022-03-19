e065dd052b5d81f187455fecb7db3e930506c954
Omer K <cleancode.py@gmail.com>
2022-03-19 10:25:57 -0400

    Add files via upload

---
+++ reversed string.py
@@ -0,0 +1,9 @@
+# REVERSED STRING
+def reverse(string):
+    reversed_string = ""
+    for i in string:
+        reversed_string = i+reversed_string
+    print("reversed string:", reversed_string)
+
+string = input("enter a string: ")
+reverse(string)
